9730ddad9c4f13114557753dc9c4b9fb6949929e
Coba <coba@cobac.eu>
2021-03-31 13:31:43 +0200

    n_operators, n_terminals

--- BayesianSR/src/describetree.jl
+++ BayesianSR/src/describetree.jl
@@ -1,10 +1,17 @@
-function flatten(node::RuleNode)
-    out = []
-    queue = [node]
-    while !isempty(queue)
-        push!(out, queue[1].ind)
-        append!(queue, queue[1].children)
-        deleteat!(queue, 1)
-    end
+function n_operators(node::RuleNode, grammar::Grammar)
+    node_types = nodetypes(grammar)
+    operator_is = findall(x -> x==1 || x==2, node_types)
+
+    nodes = flatten(node)
+    out = count(i -> in(i, operator_is), nodes)
+    return out
+end
+
+function n_terminals(node::RuleNode, grammar::Grammar)
+    node_types = nodetypes(grammar)
+    terminal_is = findall(x -> x==0, node_types)
+
+    nodes = flatten(node)
+    out = count(i -> in(i, terminal_is), nodes)
     return out
 end
