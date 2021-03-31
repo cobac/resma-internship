3ff4c9813c695f69bd04dd3c382ac2f736001d29
Coba <coba@cobac.eu>
2021-03-31 15:34:35 +0200

    n_candidates function

--- BayesianSR/src/describetree.jl
+++ BayesianSR/src/describetree.jl
@@ -15,3 +15,18 @@ function n_terminals(node::RuleNode, grammar::Grammar)
     out = count(i -> in(i, terminal_is), nodes)
     return out
 end
+
+function n_candidates(node::RuleNode, grammar::Grammar)
+    n = n_operators(node, grammar)
+
+    node_types = nodetypes(grammar)
+    operator_is = findall(x -> x==1 || x==2, node_types)
+
+    inds = [child.ind for child in node.children]
+
+    if count(i -> in(i, operator_is), inds) == 0
+        n -= 1
+    end 
+
+    return n
+end 
