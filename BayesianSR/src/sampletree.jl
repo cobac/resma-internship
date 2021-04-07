3eadaf842f8354538d143d3be6c4af86f01920d8
Coba <coba@cobac.eu>
2021-04-07 17:54:29 +0200

    Structure of tree sampling

---
+++ BayesianSR/src/sampletree.jl
@@ -0,0 +1,27 @@
+
+function grow!(node::RuleNode)
+    # sample which node
+    # get the deepness that that node :/
+    # do the thing
+end 
+
+function prune!(node::RuleNode)
+end 
+
+function delete!(node::RuleNode)
+end 
+
+function insert_node!(node::RuleNode)
+end 
+
+function re_operator!(node::RuleNode)
+end 
+
+function re_feature!(node::RuleNode)
+end 
+
+#node_index = sample(NodeLoc, tree)
+#old_node = get(tree, node_index)
+#new_node = rand(RuleNode, grammar, :Real, 3)
+#insert!(tree, node_index, new_node)
+#new_equation = get_executable(tree, grammar)
--- BayesianSR/src/sampling.jl
+++ BayesianSR/src/sampling.jl
@@ -9,56 +9,44 @@ function generateproposal()
 end 
 
 function proposetree(tree::RuleNode, grammar)
+    tree = deepcopy(tree)
     nₒ = n_operators(tree, grammar)
-    # nₜ= n_terminals(tree, grammar)
     n_cand = n_candidates(tree, grammar)
-
     # TODO: modify after implementing =lt()= operators
     # P_0 = P(tree stays the same)
     p_0 = 0
-    if p_0 > rand()
-        return tree
-    end 
-
-    tree = deepcopy(tree)
-
     # P_g = P(tree grows)
     p_g = (1 - p_0)/3 * min(1, 8/(nₒ+2))
-    if p_g > rand()
-        
-    end 
-
     # P_p = P(pruning the tree)
     p_p = (1-p_0)/3-p_g
-    if p_p > rand()
-
-    end 
-
     # P_d = P(deleting a candidate node)
     p_d = (1-p_0)/3 * n_cand/(n_cand+3)
-    if p_d > rand()
-
-    end 
-
     # P_i = P(insert a new node)
     p_i  = (1-p_0)/3 - p_d
-    if p_d > rand()
-
-    end 
-
     # P_ro = P(reassign operator)
     # P_rf = P(reassign feature)
     p_ro = p_rf = (1-p_0)/6
-    if p_ro > rand()
+    movements = [:stay , :grow, :prune, :delete, :insert, :re_operator, :re_feature]
+    weights =   [p_0   ,   p_g,    p_p,     p_d,     p_i,         p_ro,        p_rf]
+    mov = sample(movements, Weights(weights))
 
+    if mov == :stay
+        nothing
+    elseif mov == :grow
+        tree = grow!(tree)
+    elseif mov == :prune
+        tree = prune!(tree)
+    elseif mov == :delete
+        tree = delete!(tree)
+    elseif mov == :insert
+        tree = insert_node!(tree)
+    elseif mov == :re_operator
+        tree = re_operator!(tree)
+    elseif mov == :re_feature
+        tree = re_feature!(tree)
     end 
 
-    if p_rf > rand()
-
-    end 
-
-    return (eqtree = tree, movements = movements)
-
+    return (tree = tree, movement = mov)
 end 
 
 
