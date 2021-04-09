184227bdbd757fcf00e94624156bb6e27a06aef9
Coba <coba@cobac.eu>
2021-04-09 09:47:40 +0200

    candidate utils functions

--- BayesianSR/src/utils.jl
+++ BayesianSR/src/utils.jl
@@ -34,3 +34,30 @@ function sampleoperator(node::RuleNode, grammar::Grammar)
     end 
     return out
 end 
+
+function samplecandidate(node::RuleNode, grammar::Grammar)
+    out = sample(NodeLoc, node)
+    # if node is not candidate,  resample
+    target = get(node, out)
+    while !iscandidate(target, node, grammar)
+        out = sample(NodeLoc, node)
+        target = get(node, out)
+    end 
+    return out
+end 
+
+function iscandidate(target::RuleNode, root::RuleNode, grammar::Grammar)
+    node_types = nodetypes(grammar)
+    terminal_is = findall(x -> x==0, node_types)
+    operator_is = findall(x -> x==1 || x==2, node_types)
+    inds = [child.ind for child in target.children]
+    if in(target.ind, terminal_is)
+        # terminal node == false
+        return false
+    elseif target == root && count(i -> in(i, operator_is), inds) == 0
+        # root node with all terminal children == false
+        return false
+    else
+        return true
+    end 
+end 
