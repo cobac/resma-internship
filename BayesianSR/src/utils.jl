2fba4a150cad599586dda450056587ce4cbf3d81
Coba <coba@cobac.eu>
2021-04-15 19:05:15 +0200

    moved describetree.jl utils to utils.jl

--- BayesianSR/src/utils.jl
+++ BayesianSR/src/utils.jl
@@ -61,3 +61,33 @@ function iscandidate(target::RuleNode, root::RuleNode, grammar::Grammar)
         return true
     end 
 end 
+
+length(chain::Chain) = length(chain.samples)
+no_trees(chain::Chain) = length(chain.samples[1].trees)
+
+function n_operators(node::RuleNode, grammar::Grammar)
+    node_types = nodetypes(grammar)
+    operator_is = findall(x -> x==1 || x==2, node_types)
+    nodes = flatten(node)
+    out = count(i -> in(i, operator_is), nodes)
+    return out
+end
+
+function n_terminals(node::RuleNode, grammar::Grammar)
+    node_types = nodetypes(grammar)
+    terminal_is = findall(x -> x==0, node_types)
+    nodes = flatten(node)
+    out = count(i -> in(i, terminal_is), nodes)
+    return out
+end
+
+function n_candidates(node::RuleNode, grammar::Grammar)
+    n = n_operators(node, grammar)
+    node_types = nodetypes(grammar)
+    operator_is = findall(x -> x==1 || x==2, node_types)
+    inds = [child.ind for child in node.children]
+    if count(i -> in(i, operator_is), inds) == 0
+        n -= 1
+    end 
+    return n
+end 
