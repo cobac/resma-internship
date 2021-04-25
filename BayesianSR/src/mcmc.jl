a5ca6f260347d38fdddf0d73cb44504596f42faa
Coba <coba@cobac.eu>
2021-04-25 21:25:49 +0200

    Ratio definition with log-P

--- BayesianSR/src/mcmc.jl
+++ BayesianSR/src/mcmc.jl
@@ -25,25 +25,32 @@ function step!(chain::Chain)
     # Calculate R
     tree_x_proposal = evalsample(proposal, chain.x, chain.grammar)
     X_proposal = [ones(size(tree_x_proposal)[1]) tree_x_proposal]
-    ε_proposal = BigFloat.(chain.y - X_proposal * proposal.β)
-    L_proposal = prod(pdf(Normal(0, proposal.σ²), ε_proposal))
-    numerator = L_proposal * pdf(σ²_prior, proposal.σ²) 
+    ε_proposal = chain.y - X_proposal * proposal.β
+    L_proposal = sum(log.(pdf(Normal(0, proposal.σ²), ε_proposal)))
+    numerator = L_proposal +
+        log(pdf(σ²_prior, proposal.σ²)) +
+        sum([tree_p(eqtree.S, chain.grammar) for eqtree in proposal.trees]) +
+        proposal_tree.p_mov
 
     tree_x_old_sample = evalsample(old_sample, chain.x, chain.grammar)
     X_old_sample = [ones(size(tree_x_old_sample)[1]) tree_x_old_sample]
-    ε_old_sample = BigFloat.(chain.y - X_old_sample * old_sample.β)
-    L_old_sample = prod(pdf(Normal(0, old_sample.σ²), ε_old_sample))
-    denominator = L_old_sample * pdf(σ²_prior, old_sample.σ²) 
-
-    R = numerator / denominator
-
+    ε_old_sample = chain.y - X_old_sample * old_sample.β
+    L_old_sample = sum(log.(pdf(Normal(0, old_sample.σ²), ε_old_sample)))
+    denominator = L_old_sample +
+        log(pdf(σ²_prior, old_sample.σ²)) +
+        sum([tree_p(eqtree.S, chain.grammar) for eqtree in old_sample.trees]) +
+        proposal_tree.p_mov_inv
+    
+    R = exp(numerator - denominator)
+    
     # Update chain
-      α = min(1, R)
-      if (rand() < α)
-          push!(chain.samples, proposal)
-      end 
+    α = min(1, R)
+    if (rand() < α)
+        println("Updating! Movement = ", proposal_tree.movement)
+        push!(chain.samples, proposal)
+    end 
 
-    return nothing
+    return R
 end 
 
 
