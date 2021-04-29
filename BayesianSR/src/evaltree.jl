f42ff00b61a19ff770d4f66e48d672652d3772d1
Coba <coba@cobac.eu>
2021-04-29 11:54:47 +0200

    @inbounds tree evaluation

--- BayesianSR/src/evaltree.jl
+++ BayesianSR/src/evaltree.jl
@@ -3,7 +3,7 @@ function evaltree(tree::EqTree, x, grammar::Grammar)
     #= Evaluates a tree into a vector for every variable x_j =#
     n = size(x)[1]
     out = Vector{Float64}(undef, n)
-    for i in 1:n
+    @inbounds for i in 1:n
         table = tableforeval(x, i, grammar) 
         eq = ExprRules.get_executable(tree.S, grammar)
         out[i] = Core.eval(table, eq)
@@ -13,7 +13,7 @@ end
 
 function evalsample(sample::Sample, x, grammar::Grammar)
     out = Matrix{Float64}(undef, size(x)[1], length(sample.trees))
-    for j in 1:length(sample.trees)
+    @inbounds for j in 1:length(sample.trees)
         out[:, j] = evaltree(sample.trees[j], x, grammar)
     end 
     return out
@@ -23,7 +23,7 @@ function tableforeval(x, i, grammar::Grammar)
     #= Generates a symbol table ready for evaluation for x_i =#
     symboltable = ExprRules.SymbolTable(grammar)
     k = size(x)[2]
-    for m in 1:k
+    @inbounds for m in 1:k
         symboltable[Symbol("x", m)] = x[i, m]
     end 
     return symboltable
