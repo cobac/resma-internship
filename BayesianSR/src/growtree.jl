f74f812a61feb7cda00026765bf4078f62b690cb
Coba <coba@cobac.eu>
2021-05-07 14:34:38 +0200

    Temporary rand() in growtree

--- BayesianSR/src/growtree.jl
+++ BayesianSR/src/growtree.jl
@@ -6,24 +6,25 @@ Samples a branch from the prior distribution.
 The root node is at `d = 1`.
 """
 function growtree(grammar::Grammar, d::Int)
-    node_types = nodetypes(grammar)
-    node = RuleNode(0)
-    
-    # Hyper: α, β = 2, 1
-    # Prior: Uniform for operators and features
-    p₁ = 2/(1+d)
-    if p₁ > rand()
-        # Node = operator
-        node.ind = sample(findall(x -> x==1 || x==2, node_types))
-        for child in 1:node_types[node.ind]
-            push!(node.children, growtree(grammar, d+1))
-        end 
-    else
-        # Node = terminal
-        node.ind = sample(findall(x -> x==0, node_types))
-    end 
+    #  node_types = nodetypes(grammar)
+    #  node = RuleNode(0)
+    #  
+    #  # Hyper: α, β = 2, 1
+    #  # Prior: Uniform for operators and features
+    # p₁ = 2/(1+d)
+    # if p₁ > rand()
+    #     # Node = operator
+    #      node.ind = sample(findall(x -> x==1 || x==2, node_types))
+    #      for child in 1:node_types[node.ind]
+    #          push!(node.children, growtree(grammar, d+1))
+    #      end 
+    #  else
+    #      # Node = terminal
+    #      node.ind = sample(findall(x -> x==0, node_types))
+    #  end 
 
-    return node
+    # return node
+    return rand(RuleNode, grammar, :Real, 10)
 end 
 
 """
