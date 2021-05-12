8a8ad4f67225da7c30e7d1ddd06810cb84ea50f5
Coba <coba@cobac.eu>
2021-05-12 18:18:15 +0200

    Fixed Hyperparams typing

--- BayesianSR/src/BayesianSR.jl
+++ BayesianSR/src/BayesianSR.jl
@@ -11,9 +11,9 @@ Hyperparameters of a `Chain`.
 """
 @with_kw struct Hyperparams
     k = 3::Int
-    σ²_prior = InverseGamma(0.5, 0.5)::UnivariateDistribution
-    σ²_a_prior = InverseGamma(0.5, 0.5)::UnivariateDistribution
-    σ²_b_prior = InverseGamma(0.5, 0.5)::UnivariateDistribution
+    σ²_prior::UnivariateDistribution = InverseGamma(0.5, 0.5)
+    σ²_a_prior::UnivariateDistribution = InverseGamma(0.5, 0.5)
+    σ²_b_prior::UnivariateDistribution = InverseGamma(0.5, 0.5)
 end 
 
 """
