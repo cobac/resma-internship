37165e23aba95874483cfac786a5dd0d829b1229
Coba <coba@cobac.eu>
2021-05-20 20:50:39 +0200

    Better mcmc spacing

--- BayesianSR/src/mcmc.jl
+++ BayesianSR/src/mcmc.jl
@@ -5,7 +5,7 @@ Generates a new Sample of `Chain`.
 `i` is the index of the latest sample.
 `j` ∈ {1..k} is the index of the tree to be modified.
 """
-function step(chain::Chain, i::Int, j::Int ; verbose::Bool = false)
+function step(chain::Chain, i::Int, j::Int ; verbose::Bool=false)
     @unpack k, σ²_prior, σ²_a_prior, σ²_b_prior = chain.hyper
 
     # Initialize new sample
@@ -19,7 +19,7 @@ function step(chain::Chain, i::Int, j::Int ; verbose::Bool = false)
 
     # Propose a new tree
     proposal_tree = proposetree(proposal.trees[j], chain.grammar, chain.hyper,
-                                verbose = verbose)
+                                verbose=verbose)
 
     # Maybe propose a new set of LinearCoef parameters
     any_linear_operator_proposal = any_linear_operators(proposal_tree.tree)
@@ -95,14 +95,14 @@ end
 
 Samples from the posterior space `n_steps` iterations via MCMC.
 """
-function mcmc!(chain::Chain, n_steps::Int = 100; verbose::Bool = false)
+function mcmc!(chain::Chain, n_steps::Int=100; verbose::Bool=false)
     i₀ = length(chain)
     resize!(chain.samples, i₀ + n_steps)
     for i in (i₀ + 1):(i₀ + n_steps)
         j = chain.stats[:lastj] + 1
         j == no_trees(chain) + 1 ? j = 1 : nothing
         chain.stats[:lastj] = j
-        chain.samples[i] = BayesianSR.step(chain, i-1, j, verbose = verbose)
+        chain.samples[i] = BayesianSR.step(chain, i - 1, j, verbose=verbose)
     end 
 end 
 
